39601c054c3cd7557c4c4ae0511ed4395ed013e4
developer <developer@finogeeks.com>
2021-12-14 16:08:00 +0800

    version++

---
+++ FinAppletGDMap.framework/Headers/FATGDMapComponent.h
@@ -0,0 +1,19 @@
+//
+//  FATGDMapComponent.h
+//  FinAppletGDMap
+//
+//  Created by 王滔 on 2021/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FATGDMapComponent : NSObject
+
++ (void)setGDMapAppKey:(NSString *)appkey;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FinAppletGDMap.framework/Headers/FinAppletGDMap.h
@@ -0,0 +1,19 @@
+//
+//  FinAppletGDMap.h
+//  FinAppletGDMap
+//
+//  Created by 王滔 on 2021/11/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "FATGDMapComponent.h"
+
+//! Project version number for FinAppletGDMap.
+FOUNDATION_EXPORT double FinAppletGDMapVersionNumber;
+
+//! Project version string for FinAppletGDMap.
+FOUNDATION_EXPORT const unsigned char FinAppletGDMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinAppletGDMap/PublicHeader.h>
+
+
